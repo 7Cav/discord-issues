7f9b7517f3cbcc5b19d5ccbbd748086f4c5a4b9b
Adam Jarvis <ajarvis@palantir.com>
2020-06-06 21:46:33 +0400

    Move column ID to env instead

--- src/commands/CreateIssue.ts
+++ src/commands/CreateIssue.ts
@@ -2,6 +2,7 @@ import {Message} from 'discord.js';
 import * as log4js from "log4js";
 import {Command} from "../types/Command";
 import {UsesOctokit} from "../types/UsesOctokit";
+import {Optional} from "typescript-optional";
 
 // init logger
 let logger = log4js.getLogger("CreateIssue");
@@ -13,9 +14,11 @@ export class CreateIssue extends UsesOctokit implements Command {
         // 1. extract issue body
         let content: string = args.join(" ");
 
+        let projectColumnId: Optional<number> = Optional.ofNonNull(Number(process.env.GITHUB_PROJECT_COLUMN_ID))!;
+
         // 2. call axios/GH api
         await this.octokit.projects.createCard({
-            column_id: 9282033,
+            column_id: projectColumnId.get(),
             note: content
         });
     }
