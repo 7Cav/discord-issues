53d1e6df0c92edf33483f27d6ed684e110dbb263
Adam Jarvis <ajarvis@palantir.com>
2020-06-10 23:37:20 +0400

    Pass up auth from google class

--- src/types/UsesGoogle.ts
+++ src/types/UsesGoogle.ts
@@ -1,23 +1,18 @@
 import {google, GoogleApis} from 'googleapis';
 import {GoogleAuth} from "google-auth-library/build/src/auth/googleauth";
 import {Optional} from "typescript-optional";
-import fs from "fs";
 
 export abstract class UsesGoogle {
 
     readonly google: GoogleApis = google;
 
-    private readonly auth: GoogleAuth;
+    readonly auth: GoogleAuth;
     private authKeyFilePath: Optional<string | undefined> = Optional.ofNonNull(process.env.GOOGLE_PEM_FILE_PATH)!;
 
-    protected constructor() {
+    constructor() {
         this.auth = new google.auth.GoogleAuth({
-            keyFile: fs.readFileSync(this.authKeyFilePath.get()!, "utf8"),
+            keyFile: this.authKeyFilePath.get(),
             scopes: ['https://www.googleapis.com/auth/calendar'],
-        });
-
-        this.google.options({
-            auth: this.auth
         });
     }
 }
